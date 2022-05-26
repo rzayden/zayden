2d975ba947cb21d4404813f303347190207a78eb
Zayden Kelsen <105763201+rzayden@users.noreply.github.com>
2022-05-26 22:32:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("welcome to my teo reo quiz!")
+
+#chances
+chances = 1 
+print("you will have", chances, "chance to answer correctly. \n what colour is red in teo reo\n")
+
+#score
+score = 0
+
+#question 1
+question_1 = print("1) how do you say hi in teo reo?\n(a) kia ora\n(b) tena koutou\n(c) whare\n(d)
