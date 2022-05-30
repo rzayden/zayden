21d857831b2aae61a86e6a191b820a5311c2207c
Zayden Kelsen <105763201+rzayden@users.noreply.github.com>
2022-05-30 00:56:22 +0000

    i made the score work added 5 questions did the question

--- main.py
+++ main.py
@@ -2,10 +2,75 @@ print("welcome to my teo reo quiz!")
 
 #chances
 chances = 1 
-print("you will have", chances, "chance to answer correctly. \n what colour is red in teo reo\n")
+print("you will have", chances, "chance to answer correctly. \n")
 
 #score
 score = 0
 
 #question 1
-question_1 = print("1) how do you say hi in teo reo?\n(a) kia ora\n(b) tena koutou\n(c) whare\n(d)
+question_1 = print("1) what day is canada day?\n(a) july 4\n(b) july 2\n(c) july 1\n(d) july 3\n\n ")
+answer_1 = "c"
+
+for i in range(chances) :
+    answer = input("answer: ")
+    if (answer.lower() == answer_1):
+       print("correct! Good Job. \n")
+       score = score + 1
+       break
+else:
+  print("incorrect!\n ")
+  print("The correct answer is" , answer_1, "\n\n ")
+
+#question 2
+question_1 = print("1) what day is canada day?\n(a) july 4\n(b) july 2\n(c) july 1\n(d) july 3\n\n ")
+answer_1 = "c"
+
+for i in range(chances) :
+    answer = input("answer: ")
+    if (answer.lower() == answer_1):
+       print("correct! Good Job. \n")
+       score = score + 1
+       break
+else:
+  print("incorrect!\n ")
+  print("The correct answer is" , answer_1, "\n\n ")
+  #question 3
+question_1 = print("1) what day is canada day?\n(a) july 4\n(b) july 2\n(c) july 1\n(d) july 3\n\n ")
+answer_1 = "c"
+
+for i in range(chances) :
+    answer = input("answer: ")
+    if (answer.lower() == answer_1):
+       print("correct! Good Job. \n")
+       score = score + 1
+       break
+else:
+  print("incorrect!\n ")
+  print("The correct answer is" , answer_1, "\n\n ")
+  #question 4
+question_1 = print("1) what day is canada day?\n(a) july 4\n(b) july 2\n(c) july 1\n(d) july 3\n\n ")
+answer_1 = "c"
+
+for i in range(chances) :
+    answer = input("answer: ")
+    if (answer.lower() == answer_1):
+       print("correct! Good Job. \n")
+       score = score + 1
+       break
+else:
+  print("incorrect!\n ")
+  print("The correct answer is" , answer_1, "\n\n ")
+  #question 5
+question_1 = print("1) what day is canada day?\n(a) july 4\n(b) july 2\n(c) july 1\n(d) july 3\n\n ")
+answer_1 = "c"
+
+for i in range(chances) :
+    answer = input("answer: ")
+    if (answer.lower() == answer_1):
+       print("correct! Good Job. \n")
+       score = score + 1
+       break
+else:
+  print("incorrect!\n ")
+  print("The correct answer is" , answer_1, "\n\n ")
+  
