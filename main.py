2b8c1a4f229c5977445488772788e427695a2f0e
Zayden Kelsen <105763201+rzayden@users.noreply.github.com>
2022-06-16 21:56:43 +0000

    question 2

--- main.py
+++ main.py
@@ -21,4 +21,18 @@ else:
   print("incorrect!\n ")
   print("The correct answer is" , answer_1, "\n\n ")
 
-  
+  #question 2
+question_2 = print(" 2) what is hi in teo reo?\n(a) teitei\n(b) harikoa\n(c) kia ora\n(d) hei\n\n ")
+
+answer_1 = "c"
+
+for i in range(chances) :
+    answer = input("answer: ")
+    if (answer.lower() == answer_1):
+       print("correct! Good Job. \n")
+       score = score + 1
+       break
+else:
+  print("incorrect!\n ")
+  print("The correct answer is" , answer_1, "\n\n ")
+
