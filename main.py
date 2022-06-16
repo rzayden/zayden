43ecdbdf1cf964a1f0d14b73cc8d39bb00beb4fa
Zayden Kelsen <105763201+rzayden@users.noreply.github.com>
2022-06-16 21:57:11 +0000

    question 3

--- main.py
+++ main.py
@@ -36,3 +36,17 @@ else:
   print("incorrect!\n ")
   print("The correct answer is" , answer_1, "\n\n ")
 
+#question 3
+question_3 = print(" 3) what is greetings in teo reo?\n(a) ata pai\n(b) tena koutou\n(c) paru\n(d) tere\n\n ")
+
+answer_1 = "b"
+for i in range(chances) :
+    answer = input("answer: ")
+    if (answer.lower() == answer_1):
+       print("correct! Good Job. \n")
+       score = score + 1
+       break
+else:
+  print("incorrect!\n ")
+  print("The correct answer is" , answer_1, "\n\n ")
+
